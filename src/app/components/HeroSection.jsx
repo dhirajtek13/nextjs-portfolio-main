d64f8e129a8a9225bf67f2d761fb078a79db517b
dhirajtek13 <dhirajtek13@gmail.com>
2024-01-10 20:46:38 +0530

    resend api commented

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -66,7 +66,7 @@ const HeroSection = () => {
             <Image
               src="/images/hero-image6.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 scale-x-[-1]"
               width={300}
               height={300}
             />
