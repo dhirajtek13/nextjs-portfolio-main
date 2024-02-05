132b0dd64346cdfbda9bb06320f2e22cf3941df2
dhirajtek13 <dhirajtek13@gmail.com>
2024-02-05 22:19:11 +0530

    few projects added

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -4,62 +4,7 @@ import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
 
-const projectsData = [
-  {
-    id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 2,
-    title: "Potography Portfolio Website",
-    description: "Project 2 description",
-    image: "/images/projects/2.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 3,
-    title: "E-commerce Application",
-    description: "Project 3 description",
-    image: "/images/projects/3.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Food Ordering Application",
-    description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 5,
-    title: "React Firebase Template",
-    description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 6,
-    title: "Full-stack Roadmap",
-    description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-];
+import { projectsData } from "./utils/projectsData";
 
 const ProjectsSection = () => {
   const [tag, setTag] = useState("All");
@@ -70,6 +15,17 @@ const ProjectsSection = () => {
     setTag(newTag);
   };
 
+  // Extracting tags from each project
+    const allTags = projectsData.reduce((tags, project) => {
+      tags.push(...project.tag);
+      return tags;
+    }, []);
+
+    // Creating a unique array of tags
+    const allTagsArr = [...new Set(allTags)];
+
+
+  // const allTagsArr = projectsData;
   const filteredProjects = projectsData.filter((project) =>
     project.tag.includes(tag)
   );
@@ -85,21 +41,35 @@ const ProjectsSection = () => {
         My Projects
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-        <ProjectTag
+        {allTagsArr.map((tag_name) => (
+            <ProjectTag
+            key={tag_name}
+            onClick={handleTagChange}
+            name={tag_name}
+            isSelected={tag === tag_name}
+          />
+          )
+        )}
+        {/* <ProjectTag
           onClick={handleTagChange}
-          name="All"
-          isSelected={tag === "All"}
+          name="Personal"
+          isSelected={tag === "Personal"}
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
+          name="Company"
+          isSelected={tag === "Company"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Interview"
+          isSelected={tag === "Interview"}
         />
         <ProjectTag
           onClick={handleTagChange}
           name="Mobile"
           isSelected={tag === "Mobile"}
-        />
+        /> */}
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
